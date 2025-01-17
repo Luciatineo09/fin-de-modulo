1137f531f1f6bcab4fee1b20a5ea2a5449f238c8
Luciatineo09 <amaliatineo09@hotmail.com>
2025-01-17 17:38:23 -0500

    taks

--- SD-12-1 Assessment-Individual/sd-l6-1.0/task2.js
+++ SD-12-1 Assessment-Individual/sd-l6-1.0/task2.js
@@ -1,2 +1,5 @@
 // Task 2: listUsers()
-
+export function listUsers() {
+    let serverURL = "http://localhost:3000";
+    fetch(serverURL + "/users").then(response => response.json()).then(data => console.log(data));
+}
